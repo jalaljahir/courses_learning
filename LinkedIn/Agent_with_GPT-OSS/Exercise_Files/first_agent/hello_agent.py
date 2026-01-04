34417faf6794d84ad782e00c37ef893823ecfbaf
jalaljahir <jalaljahir@hotmail.com>
2026-01-04 08:52:20 -0600

    linkedin agent with gpt-oss

--- LinkedIn/Agent_with_GPT-OSS/Exercise_Files/first_agent/hello_agent.py
+++ LinkedIn/Agent_with_GPT-OSS/Exercise_Files/first_agent/hello_agent.py
@@ -5,14 +5,14 @@ def run_agent():
     # Understand Intent
     user_name = input ("What is your name?")
     # Plan
-    print("Agent: I should use the create_greeting tool.")
+    print("Optimus: I should use the create_greeting tool.")
     # Use Tools
     greeting = create_greeting (user_name)
     # Reflect
     if greeting:
-        print(f"Agent: {greeting}")
+        print(f"Optimus: {greeting}")
     else:
-        print("Agent: I failed to create a greeting")
+        print("Optimus: I failed to create a greeting")
 
 if __name__ == "__main__":
     run_agent ()
